e29117bc90ab57264630a4dc3fcacb261889ac65
Thomas Littrell <Thomas.Littrell@quantumblack.com>
2020-06-10 16:03:38 -0400

    refactor dqn function to take in agent

--- src/dqn.py
+++ src/dqn.py
@@ -3,8 +3,18 @@ from collections import deque
 import numpy as np
 import torch
 
+from src.agent import Agent
 
-def dqn(n_episodes=10, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+
+def dqn(
+    agent: Agent,
+    env,
+    brain_name,
+    n_episodes: int = 10,
+    eps_start: float = 1.0,
+    eps_end: float = 0.01,
+    eps_decay: float = 0.995,
+):
     """Deep Q-Learning.
 
     Params
@@ -17,7 +27,7 @@ def dqn(n_episodes=10, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     """
     scores = []  # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
-    eps = eps_start  # initialize epsilon
+    eps = eps_start
     for i_episode in range(1, n_episodes + 1):
         env_info = env.reset(train_mode=True)[brain_name]
         state = env_info.vector_observations[0]
@@ -38,22 +48,15 @@ def dqn(n_episodes=10, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
         scores.append(score)  # save most recent score
         eps = max(eps_end, eps_decay * eps)  # decrease epsilon
         print(
-            "\rEpisode {}\tAverage Score: {:.2f}".format(
-                i_episode, np.mean(scores_window)
-            ),
+            f"\rEpisode {i_episode}\tAverage Score: {np.mean(scores_window):.2f}",
             end="",
         )
         if i_episode % 100 == 0:
-            print(
-                "\rEpisode {}\tAverage Score: {:.2f}".format(
-                    i_episode, np.mean(scores_window)
-                )
-            )
+            print(f"\rEpisode {i_episode}\tAverage Score: {np.mean(scores_window):.2f}")
         if np.mean(scores_window) >= 13.0:
             print(
-                "\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}".format(
-                    i_episode - 100, np.mean(scores_window)
-                )
+                f"\nEnvironment solved in {i_episode-100:d} episodes!\tAverage Score:"
+                f" {np.mean(scores_window):.2f}"
             )
             torch.save(agent.qnetwork_local.state_dict(), "checkpoint.pth")
             break
